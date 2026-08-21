da186eece671cad9f0e0a368eefb2438d39bf4d0
dhruv85-del <rdhruv714@gmail.com>
2026-08-21 21:06:52 +0530

    Time: 172 ms (11.76%) | Memory: 43.4 MB (94.12%) - LeetSync

---
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        long low = 1;
+        long high = 1L * Arrays.stream(coins).min().getAsInt() * k;
+
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+
+            if (count(mid, coins) >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private long count(long x, int[] coins) {
+        int n = coins.length;
+        long result = 0;
+
+        // Try every subset of coins
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long lcm = 1;
+            int bits = 0;
+            boolean valid = true;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    bits++;
+
+                    long g = gcd(lcm, coins[i]);
+                    lcm = lcm / g * coins[i];
+
+                    // LCM is already bigger than x,
+                    // so it contributes 0 multiples.
+                    if (lcm > x) {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!valid) continue;
+
+            long multiples = x / lcm;
+
+            if (bits % 2 == 1) {
+                result += multiples;
+            } else {
+                result -= multiples;
+            }
+        }
+
+        return result;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
